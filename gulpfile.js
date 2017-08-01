8ba512bf145ab07cd63d66020631a0e6e35741d2
Jordan Clay <jordan.clay@blackburn.edu>
2017-08-01 12:03:31 -0500

    test push

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 var gulp = require('gulp');
-var pug = require('gulp-pug');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-csso');
 
 gulp.task('html', function(){
-  return gulp.src('client/templates/*.pug')
-    .pipe(pug())
+  return gulp.src('src/html/**/*.html')
     .pipe(gulp.dest('build/html'))
 });
 
